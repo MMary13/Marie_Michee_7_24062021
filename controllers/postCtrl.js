c246c83e8a12ca869118aa62d82acafe3777a930
Marie <marie.michee@gmail.com>
2021-07-28 18:31:34 +0200

    Update create/modify post method to save image

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -4,15 +4,16 @@ const Post = require('../models/Post');
 
 //Create Post method--------
 exports.newPost = async function(req, res, next) {
-    if(postValidation(req.body,res)) {
+    const postObject = JSON.parse(req.body.post);
+    if(postValidation(postObject,res)) {
         const userId = GET_USERID_FROM_TOKEN(req);
-        const postObject = req.file ?
+        const newPost = req.file ?
             {
-            ...req.body,
+            ...postObject,
             user_id: userId,
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-            } : {...req.body, user_id: userId};
-        Post.create(postObject)
+            } : {...postObject, user_id: userId};
+        Post.create(newPost)
         .then(() => {
             res.status(201).json({ message: 'Publication créée !' })
         })
@@ -47,15 +48,16 @@ exports.getAllPosts = async function (req,res,next) {
 //Update a Post method-------------
 exports.modifyPost = async function (req,res,next) {
     if(authorizedToModifyThisPost(req)) {
-        if(postValidation(req.body,res)) {
+        const postObject = JSON.parse(req.body.post);
+        if(postValidation(postObject,res)) {
             const userId = GET_USERID_FROM_TOKEN(req);
-            const postObject = req.file ?
+            const updatedPost = req.file ?
                 {
-                ...req.body,
+                ...postObject,
                 user_id: userId,
                 imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-                } : {...req.body, user_id: userId};
-            Post.update(postObject, {where: { id:req.params.id }})
+                } : {...postObject, user_id: userId};
+            Post.update(updatedPost, {where: { id:req.params.id }})
                 .then(updatedRows => {
                     res.status(200).json({ message:'Publication mise à jour, lignes modifiées: '+updatedRows })
                 })
@@ -90,6 +92,7 @@ exports.deleteOnePost = async function (req,res,next) {
 //User validation----------
 function postValidation(post,res) {
     console.log(post);
+    console.log("Title: "+post.title);
     if(post.title!=null) {
         if (post.content!=null) {
             return true;
